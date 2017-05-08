4fb1f5a7285b03eba4953cdb71d68a787d1b837d
Jean-Philippe Player <jpplayer@gmail.com>
2017-05-08 18:30:31 +0000

    Switched to webhdfs

--- scripts/utils/create-user-hdfs.sh
+++ scripts/utils/create-user-hdfs.sh
@@ -6,8 +6,13 @@ if [[ -z $1 ]]; then
 	exit 1
 fi
 USERNAME=$1
+#TODO: Obtain the active Namenode from Ambari
+HOSTNAME=`hostname -f`
 echo "Creating /user/${USERNAME} folder in HDFS."
 su hdfs -c 'princ=$(klist -kt /etc/security/keytabs/hdfs.headless.keytab | grep @ | tail -1 | awk "{print \$4}" ); kinit -kt /etc/security/keytabs/hdfs.headless.keytab $princ'
-su hdfs -c "hdfs dfs -mkdir -p /user/${USERNAME}"
-su hdfs -c "hdfs dfs -chown ${USERNAME} /user/${USERNAME}"
+#su hdfs -c "hdfs dfs -mkdir -p /user/${USERNAME}"
+su hdfs -c "curl --negotiate -u : -b ~/cookiejar.txt -c ~/cookiejar.txt -X PUT 'http://${HOSTNAME}:50070/webhdfs/v1/user/${USERNAME}?op=MKDIRS'"
+#su hdfs -c "hdfs dfs -chown ${USERNAME} /user/${USERNAME}"
+su hdfs -c "curl --negotiate -u : -b ~/cookiejar.txt -c ~/cookiejar.txt -X PUT 'http://${HOSTNAME}:50070/webhdfs/v1/user/${USERNAME}?op=SETOWNER&owner=${USERNAME}'"
+
 
